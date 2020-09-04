0613e78829302ada19c510dc94f1c5bb1c3411b3
MIHBEL <m-119@ya.ru>
2020-09-04 20:11:13 +0300

    py 200

---
+++ PY_200.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8
+
+#
+# Курс DEV-PY200. Объектно-ориентированное программирование на языке Python
+# Тема 1.1 Основы ООП. Понятие класса, объекта. Создание экземпляра класса
+
+# Лабораторная работа № 1.1 (4 ак.ч.)
+
+# Слушатель (ФИО): Беляков М.А..
+
+# ---------------------------------------------------------------------------------------------
+# Понятие класса, объекта (стр. 1-22)
+
+# 1. Создайте класс Glass с атрибутами capacity_volume и occupied_volume
+#    Обязательно проверяйте типы (TypeError) и значения переменных (ValueError)
+
+
+
+# 2. Создайте два и более объектов типа Glass
+#    Измените и добавьте в любой стакан любое кол-во воды (через атрибуты)
+#    Убедитесь, что у других объектов Glass атрибуты экземпляра класса не изменились.
+
+from Glass import Glass
+glass1: Glass = Glass()
+glass2: Glass = Glass()
+print(glass1)
+print(glass2)
+print([glass1, glass2])
+
+# 3. Создайте класс GlassDefaultArg (нужен только __init__) c аргументом occupied_volume
+#    По умолчанию occupied_volume равен нулю. Создайте два объекта с 0 и 200
+#    Обязательно проверяйте типы (TypeError) и значения переменных (ValueError)
+
+from GlassDefaultArg import GlassDefaultArg
+GlassDefaultArg1: GlassDefaultArg = GlassDefaultArg()
+GlassDefaultArg1: GlassDefaultArg = GlassDefaultArg(200)
+
+# 4. Создайте класс GlassDefaultListArg (нужен только __init__)
+#    c аргументами capacity_volume, occupied_volume.
+#    Пусть аргументом по умолчанию для __init__ occupied_volume = []. Будет список.
+#    Попробуйте создать 3 объекта, которые изменяют occupied_volume.append(2) внутри __init__.
+#    Создавайте объект GlassDefaultListArg только с одним аргументом capacity_volume.
+#    Опишите результат.
+#    Подсказка: можно ли использовать для аргументов по умолчанию изменяемые типы?
+
+from GlassDefaultArg import GlassDefaultArg
+GlassDefaultArg1: GlassDefaultArg = GlassDefaultArg()
+GlassDefaultArg1: GlassDefaultArg = GlassDefaultArg(200)
+
+# 5. Создайте класс GlassAddRemove, добавьте методы add_water, remove_water
+#    Обязательно проверяйте типы (TypeError) и значения переменных (ValueError)
+#    Вызовите методы add_water и remove.
+#    Убедитесь, что методы правильно изменяют атрибут occupied_volume.
+
+
+# 6. Создайте три объекта типа GlassAddRemove,
+#    вызовите функцию dir для трёх объектов и для класса GlassAddRemove.
+#    а. Получите типы объектов и класса
+#    б. Проверьте тип созданного объекта.
